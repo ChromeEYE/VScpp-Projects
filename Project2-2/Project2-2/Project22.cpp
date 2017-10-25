1c67e5d224074b9c7ed869dbfb05941846eb3157
SUNRISE <hgadkuhin@gmail.com>
2017-10-25 19:46:36 +0900

    20171025

---
+++ Project2-2/Project2-2/Project22.cpp
@@ -0,0 +1,90 @@
+﻿// Project22.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "stdafx.h"
+
+
+
+
+int main()
+{
+	FILE* fl;
+	char labyrinth[15][35];
+	char buff[256];
+	bool u, d, r, l;
+
+	if ((fl = fopen("labyrinth.txt", "rt")) == NULL)
+	{
+		printf("file open error!");
+		return -1;
+	}
+
+	fgets(buff, 256, fl);  printf("横　%s", buff);
+	fgets(buff, 256, fl);  printf("縦　%s", buff);
+		
+	for (int i = 0; i < 15; i++)
+	{
+		fgets(buff, 256, fl);
+		for (int j = 0; j < 35; j++)
+		{
+			switch (buff[j])
+			{
+			case '0':
+				labyrinth[i][j] = '0';
+				break;
+			case '1':
+				labyrinth[i][j] = '1';
+				break;
+			case '2':
+				labyrinth[i][j] = '2';
+				break;
+			case '3':
+				labyrinth[i][j] = '3';
+				break;
+			}
+		}
+	}
+
+	fclose(fl);
+
+	for (int i = 0; i < 15; i++)
+	{
+		for (int j = 0; j < 35; j++)
+		{
+			switch (labyrinth[i][j])
+			{
+			case 0:
+				printf("　");
+				break;
+			case 1:
+				if (i != 0)  u = (labyrinth[i - 1][j] == '1');
+				if (i != 15 - 1)  d = (labyrinth[i + 1][j] == '1');
+				if (i != 0)  l = (labyrinth[i][j - 1] == '1');
+				if (i != 35 - 1)  r = (labyrinth[i][j + 1] == '1');
+
+				if (u && d && l &&r)  printf("┼");
+				else if (!u && d && l && r) printf("┬");
+				else if (u && !d && l && r) printf("┴");
+				else if (u && d && !l && r) printf("├");
+				else if (u && d && l && !r) printf("┤");
+				else if (!u && !d && l && r) printf("─");
+				else if (!u && d && !l && r) printf("┌");
+				else if (!u && d && l && !r) printf("┐");
+				else if (u && !d && !l && r) printf("└");
+				else if (u && !d && l && !r) printf("┘");
+				else if (u && d && !l && !r) printf("│");
+			case 2:
+				printf("○");
+				break;
+			case 3:
+				printf("×");
+				break;
+			}
+		}
+		printf("\n");
+	}
+
+    return 0;
+}
+
